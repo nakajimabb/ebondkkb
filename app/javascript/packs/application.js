2fa73c3762c767acb3f943c127cfd7ebcb4df26a
中島真一 <nakajimabb@gmail.com>
2020-03-27 14:58:49 +0900

    install react for rails and react-rails gem $ rails webpacker:install $ rails webpacker:install:react $ rails generate react:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,25 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start();
-require("@rails/activestorage").start();
-require("channels");
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
 
-require("admin-lte/dist/css/adminlte");
-require("admin-lte/plugins/fontawesome-free/css/all");
-const images = require.context('../images/', true);
+require('admin-lte/dist/css/adminlte');
+require('fontawesome');
 
-require("admin-lte/plugins/jquery/jquery");
-require("admin-lte/plugins/bootstrap/js/bootstrap");
-require("admin-lte/dist/js/adminlte");
-
+require('jquery');
+require('bootstrap');
+require('admin-lte/dist/js/adminlte');
+require('react-select');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true);
+const imagePath = (name) => images(name, true);
+// Support component names relative to this directory:
+var componentRequireContext = require.context('components', true);
+var ReactRailsUJS = require('react_ujs');
+ReactRailsUJS.useContext(componentRequireContext);
