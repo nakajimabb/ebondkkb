1d495b429c9310bc028d443e49f2d918424d9edd
中島真一 <nakajimabb@gmail.com>
2020-04-01 17:19:46 +0900

    install typescript $ rails webpacker:install:typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker');
+const typescript = require('./loaders/typescript');
 
 const webpack = require('webpack');
 
+environment.loaders.prepend('typescript', typescript);
 module.exports = environment;
 
 environment.plugins.append(
